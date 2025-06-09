bd54efe26e599e86ace1b0d507bf3d5230ccbe62
zenyyang <2022147men@aupp.edu.kh>
2025-06-09 11:18:02 -0400

    fix: map unique keys

--- app/components/Home/News.tsx
+++ app/components/Home/News.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import PostPreview from "../PostPreview";
 
 export interface PostMetaData {
@@ -14,18 +15,23 @@ async function News() {
   return (
     <section className="py-16">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900">
-          Latest News
-        </h2>
+        <div className="w-full flex items-center justify-between">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900">
+            Latest News
+          </h2>
+          <Link
+            href="https://gcc-articles.vercel.app/articles"
+            className="hover:underline font-semibold text-sm"
+            target="_blank"
+          >
+            View all
+          </Link>
+        </div>
         <div className="mt-8">
           {articles.length > 0 &&
             articles.slice(0, 5).map((post: PostMetaData) => (
               <>
                 <PostPreview key={post.slug} {...post} />
-                <div
-                  key={post.title}
-                  className="w-full h-[1px] bg-gray-300 mb-4"
-                />
               </>
             ))}
         </div>
--- app/components/PostPreview.tsx
+++ app/components/PostPreview.tsx
@@ -15,24 +15,27 @@ const PostPreview = ({ slug, title, date, subtitle }: Props) => {
   const formattedDate = format(new Date(date), "MMMM dd, yyyy");
 
   return (
-    <div className="mb-2 flex items-start justify-between">
-      <div>
-        <Link
-          href={`https://gcc-articles.vercel.app/articles/${slug}`}
-          className="hover:underline"
-        >
-          <h2 className="font-bold text-1xl truncate max-w-64 md:max-w-xl lg:max-w-3xl">
-            {title}
-          </h2>
-        </Link>
-        <h3 className="text-sm line-clamp-2 max-w-64 md:max-w-xl lg:max-w-3xl">
-          {subtitle}
+    <>
+      <div className="mb-2 flex items-start justify-between">
+        <div>
+          <Link
+            href={`https://gcc-articles.vercel.app/articles/${slug}`}
+            className="hover:underline"
+          >
+            <h2 className="font-bold text-1xl truncate max-w-64 md:max-w-xl lg:max-w-3xl">
+              {title}
+            </h2>
+          </Link>
+          <h3 className="text-sm line-clamp-2 max-w-64 md:max-w-xl lg:max-w-3xl">
+            {subtitle}
+          </h3>
+        </div>
+        <h3 className="text-xs font-medium text-[#4f576c] dark:text-gray-400 pt-3">
+          {formattedDate}
         </h3>
       </div>
-      <h3 className="text-xs font-medium text-[#4f576c] dark:text-gray-400 pt-3">
-        {formattedDate}
-      </h3>
-    </div>
+      <div className="w-full h-[1px] bg-gray-300 mb-4" />
+    </>
   );
 };
 
