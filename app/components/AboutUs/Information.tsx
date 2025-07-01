56c45c88ab4a9a1419a61b7487e9994489c0d323
zenyyang <2022147men@aupp.edu.kh>
2025-07-01 11:31:49 -0400

    fix: redo about page

--- app/components/AboutUs/Information.tsx
+++ app/components/AboutUs/Information.tsx
@@ -1,67 +1,102 @@
+import Image from "next/image";
 import React from "react";
 
 function Information() {
   return (
-    <div className="px-4 md:px-16 pt-20">
-      <div className="container w-full flex flex-col items-center justify-center">
-        <h3 className="text-3xl lg:text-4xl font-bold text-gray-900">
-          More Information
-        </h3>
-        <p className="lg:text-lg mt-2 text-center  text-gray-600">
-          We have extensive experience in the field of finance, property and
-          information technology. We bring elite industry technique to this full
-          of life country- Cambodia. We make our financial and property
-          businesses thrive and develop healthily with the mix of international
-          visions and local opportunities. In the near future, we will have more
-          major projects coming up, embody our corporate vision and mission
-          further. With the development of “One Belt One Road,” it is our
-          pleasure to work with Cambodians, to build a beautiful home and famous
-          international metropolis.{" "}
-        </p>
-      </div>
-      <div className="container md:grid grid-cols-2 gap-2 text-primary">
-        <div className="col-span-1 relative w-full h-[300px]  md:h-[350px] mt-20 bg-[#192A4B]">
-          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-65 -z-10"></div>
-          <h2 className="absolute top-5 left-5 w-full h-full text-lg lg:text-2xl font-semibold">
-            Corporate Vision
-          </h2>
-          <p className="absolute top-16 left-5 w-[80%] h-full  lg:text-lg font-medium">
-            We commit to build an elite Cambodian based enterprise. We delicate
-            to the economic development by acting as a bridge connecting the
-            financial and property investment between ASEAN and “One Belt One
-            Road.” At the same time, we bring goods to Cambodian civilians. We
-            pay back to the community with our kindness.
+    <div className="px-4 md:px-16 py-20">
+      <div className="container w-full flex items-center justify-center gap-6">
+        <div className="w-full space-y-5 mr-16">
+          <p>
+            Golden Cambodia Century Group is Cambodia&apos;s most diversified
+            international conglomerate, with business operations spanning across
+            Southeast Asia. Upholding the principles of continuous innovation,
+            inheritance of experience, and the continuation of entrepreneurial
+            spirit, the Group is committed to driving Cambodia&apos;s
+            sustainable development and economic growth as its primary mission.
+          </p>
+          <p>
+            The Group&apos;s diversified portfolio includes real estate
+            development, asset management, financial investment, land
+            investment, media, food and beverage, and leisure and entertainment.
+            As one of Cambodia&apos;s largest property developers, the Group
+            leads local real estate development and investment, with landmark
+            projects such as the high-end Borey NADI by GC City, shopping mall
+            GC Orussey Market, the international business-residential complex
+            CEO Center, and the shopping complex SOHO Mall.
+          </p>
+          <p>
+            To date, Golden Cambodia Century has accumulated over 3,000 hectares
+            of land reserves in Cambodia and holds assets exceeding USD 3
+            billion. GCC continuously seeks new opportunities, expanding beyond
+            its core real estate and financial sectors to develop complementary
+            lifestyle industries in Cambodia. In line with global trends, the
+            Group has introduced a variety of international culinary cultures to
+            the local market, such as Luk Hot pot (Hong Kong-style hotpot) and
+            Bars, striving to create a more internationalized and enriched
+            living experience for the Cambodian people.
           </p>
         </div>
-        <div className="col-span-1 relative w-full h-[300px] md:h-[350px] mt-2 md:mt-20 bg-[#192A4B]">
-          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-65 -z-10"></div>
-          <h2 className="absolute top-5 left-5 w-full h-full text-lg lg:text-2xl font-semibold">
-            Corporate Mission
-          </h2>
-          <p className="absolute top-16 left-5 w-[80%] h-full lg:text-lg font-medium">
-            We build an integrated innovative enterprise of finance, real estate
-            and technology with the mix of international business wisdom and
-            local custom. We provide local jobs, stimulate local economy and
-            attract international capital with profound management system. We
-            contribute local disadvantaged community.
-          </p>
+        <div className="w-2/3 relative">
+          <Image
+            src="/hongkong/hongkong.avif"
+            alt="about"
+            width={1080}
+            height={1920}
+            className="w-full h-[700px] object-cover"
+          />
+          <div className="w-14 h-40 bg-[#1F2C57] absolute top-10 -left-14 shadow-2xl shadow-black text-primary font-medium text-center flex items-center justify-center">
+            <p>About Us</p>
+          </div>
         </div>
       </div>
-      <div className="container relative w-full h-[400px] md:h-[300px] mt-2 bg-[#192A4B] text-primary md:text-center">
-        <div className="absolute top-0 left-0 w-full h-full bg-black opacity-65 -z-10"></div>
-        <h2 className="absolute top-5 left-5 md:left-0 w-full h-full text-lg lg:text-2xl font-semibold">
-          More about Cambodia
-        </h2>
-        <p className="absolute top-16 left-5 md:left-0 w-[80%] md:w-full h-full lg:text-lg font-medium md:px-20">
-          Cambodia, officially the Kingdom of Cambodia, is located in the
-          southern portion of the Indochina peninsula in Southeast Asia,
-          bordered by Thailand to the northwest, Laos to the northeast, Vietnam
-          to the east and the Gulf of Thailand to the south. It is 181,035
-          square kilometres in area. Cambodia has three main economic centres:
-          Phnom Penh (Capital), Siem Reap and Sihanoukville. Other large cities
-          include Takeo, Srey Saophon, Kompong Cham, Poipet, Battambang and
-          Kampot.
-        </p>
+      <div className="mt-20">
+        <div className="relative w-full text-white">
+          <Image
+            src="/about/angkor.avif"
+            alt="indepedence"
+            width={1920}
+            height={1080}
+            className="h-[500px] object-top object-cover -z-10"
+          />
+          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-45 " />
+          <div className="z-50">
+            <h2 className="absolute top-16 left-5 md:left-14 w-full h-full text-lg lg:text-2xl font-semibold">
+              Coporate Vision
+            </h2>
+            <p className="absolute top-28 left-5 md:left-14 w-[80%] md:w-1/2 h-full lg:text-lg font-medium ">
+              To become a leading enterprise rooted in Cambodia, establishing a
+              financial and real estate investment bridge that connects ASEAN
+              and the Belt and Road Initiative, contributing to economic
+              development. At the same time, we are committed to bringing
+              kindness and positive experiences to the people of Cambodia,
+              giving back to society with compassion.
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center gap-10">
+          <Image
+            src="/about/teamm.jpg"
+            alt="team"
+            width={1080}
+            height={1080}
+            className="aspect-square object-cover"
+          />
+          <div className="w-full">
+            <h2 className="text-lg lg:text-2xl font-semibold">
+              Coporate Mission
+            </h2>
+            <p className="mt-4">
+              To integrate international business acumen with local culture and
+              traditions, building an innovative enterprise that combines
+              finance, real estate, and technology. Through a comprehensive
+              management system, we aim to create local employment
+              opportunities, stimulate the domestic economy, and attract
+              international capital. At the same time, we are dedicated to
+              supporting underprivileged communities in Cambodia and showing
+              genuine care for society.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
